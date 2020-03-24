ecc8fa9f312583d70a86bc5541b441d6fce8838d
roman <semisotnovroman2018@Yandex.ru>
2020-03-24 17:13:25 +0300

    vx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,29 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+<body>
 
-        <title>NFC</title>
+<p>Click the button to get your coordinates.</p>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+<button onclick="getLocation()">Try It</button>
 
-    </head>
-   <!-- <body>
-        <div id="app">
-            <main-app/>
-        </div>
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>-->
+<p id="demo"></p>
 
+<script>
+    var x = document.getElementById("demo");
 
-    <script>
-        alert("kek");
-        var x = document.getElementById("demo");
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else {
-                x.innerHTML = "Geolocation is not supported by this browser.";
-            }
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
         }
+    }
 
-        function showPosition(position) {
-            x.innerHTML = "Latitude: " + position.coords.latitude +
-                "<br>Longitude: " + position.coords.longitude;
-        }
-    </script>
+    function showPosition(position) {
+        x.innerHTML="Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    }
+</script>
+
+</body>
 </html>
